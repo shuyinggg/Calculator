6e35d83b0a798ea9f06ed00068fdf20262310ecb
Michael Lee <mlee42@cs.washington.edu>
2017-09-17 14:20:45 -0700

    Add getOrDefault to dictionary interface; add back 'public' keyword

--- src/main/java/datastructures/interfaces/IDictionary.java
+++ src/main/java/datastructures/interfaces/IDictionary.java
@@ -11,35 +11,52 @@ public interface IDictionary<K, V> {
      *
      * @throws NoSuchKeyException if the dictionary does not contain the given key.
      */
-    V get(K key);
+    public V get(K key);
+
+    /**
+     * Returns the value corresponding to the given key, if the key exists in the map.
+     *
+     * If the key does *not* contain the given key, returns the default value.
+     *
+     * Note: This method does not modify the map in any way. The interface also
+     *       provides a default implementation, but you may optionally override
+     *       it with a more efficient version.
+     */
+    public default V getOrDefault(K key, V defaultValue) {
+        if (this.containsKey(key)) {
+            return this.get(key);
+        } else {
+            return defaultValue;
+        }
+    }
 
     /**
      * Adds the key-value pair to the dictionary. If the key already exists in the dictionary,
      * replace its value with the given one.
      */
-    void put(K key, V value);
+    public void put(K key, V value);
 
     /**
      * Remove the key-value pair corresponding to the given key from the dictionary.
      *
      * @throws NoSuchKeyException if the dictionary does not contain the given key.
      */
-    V remove(K key);
+    public V remove(K key);
 
     /**
      * Returns 'true' if the dictionary contains the given key and 'false' otherwise.
      */
-    boolean containsKey(K key);
+    public boolean containsKey(K key);
 
     /**
      * Returns the number of key-value pairs stored in this dictionary.
      */
-    int size();
+    public int size();
 
     /**
      * Returns 'true' if this dictionary is empty and 'false' otherwise.
      */
-    default boolean isEmpty() {
+    public default boolean isEmpty() {
         return this.size() == 0;
     }
 }
